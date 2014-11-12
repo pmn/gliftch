e2b41fe8089f2ffe45bcd9031d55032fa97a02ab
Paul Nakata <pmn@github.com>
2014-11-12 17:34:50 +0100

    Basic gif reversing

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"image/gif"
   "io/ioutil"
+  "os"
 )
 
-func GifToImages(in []byte) *gif.GIF {
+func GifToImages(in []byte) gif.GIF {
 	reader := bytes.NewReader(in)
 	images, err := gif.DecodeAll(reader)
 	if err != nil {
 		panic("Couldn't decode gif")
 	}
 
-	return images
+	return *images
 }
 
 func frameReport(g *gif.GIF) {
@@ -25,7 +26,7 @@ func frameReport(g *gif.GIF) {
 
 // Get a GIF from a file
 func fromFile(name string) []byte {
-	b,err := ioutil.ReadFile(name)
+	b, err := ioutil.ReadFile(name)
   if err != nil {
     panic(err)
   }
@@ -38,9 +39,38 @@ func fromURL(url string) []byte {
 	return b
 }
 
+func reverse(g gif.GIF) *gif.GIF {
+  var out gif.GIF
+  for i := len(g.Image); i > 0; i--  {
+    out.Image = append(out.Image, g.Image[i-1])
+    out.Delay = append(out.Delay, g.Delay[i-1])
+  }
+
+  frameReport(&out)
+
+  return &out
+}
+
+func toFile(name string, g *gif.GIF) {
+  f, err := os.Create(name)
+  if err != nil {
+    panic(err)
+  }
+
+  defer f.Close()
+  err = gif.EncodeAll(f, g)
+
+  if err != nil {
+    panic(err)
+  }
+
+}
+
+
 func main() {
 	fmt.Println("Running!")
   b := fromFile("test/in.gif")
   i := GifToImages(b)
-  frameReport(i)
+  out := reverse(i)
+  toFile("test/out.gif", out)
 }
